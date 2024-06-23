4df75ecefa5e9dd196e091fcf130530bb5c1813a
tic <navi.sut@gmail.com>
2024-06-23 19:33:09 +0330

    New Windows, New Code. Getting Better

--- src/main/java/ir/tic/clouddc/pm/Pm.java
+++ src/main/java/ir/tic/clouddc/pm/Pm.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class Pm implements PmAction {    // new Task style
+public abstract class Pm {    // new Task style
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
